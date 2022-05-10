d081e70d2780c6499b5dad1883e606d6a292f7b7
acolistro <arcolistro@gmail.com>
2022-05-10 06:43:57 -0700

    Add image resource and function

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,16 +3,14 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.Paragraph
 import androidx.compose.ui.text.style.TextAlign
@@ -44,31 +42,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArticleGreetingWithText(title: String, paragraph: String, paragraphTwo: String) {
-    Text(
-        text = title,
-        fontSize = 24.sp,
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentWidth(Alignment.CenterHorizontally)
-            .padding(all = 16.dp)
+    Column {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(all = 16.dp)
         )
-    Text(
-        text = paragraph,
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentWidth(Alignment.CenterHorizontally)
-            .padding(start = 16.dp, end = 16.dp),
-        textAlign = TextAlign.Justify
+        Text(
+            text = paragraph,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
 
-    )
-    Text(
-        text = paragraphTwo,
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentWidth(Alignment.CenterHorizontally)
-            .padding(all = 16.dp),
-        textAlign = TextAlign.Justify
-    )
+        )
+        Text(
+            text = paragraphTwo,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(all = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun ArticleGreetingWithImage(title: String, paragraph: String, paragraphTwo: String) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Box {
+        
+    }
 }
 
 @Preview(showBackground = true)
